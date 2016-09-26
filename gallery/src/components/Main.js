a80d5d7ddc99ae10b7e5f7b3c9a85fbe0288f706
ycao <caoyue@eastmoney.com>
2016-09-26 17:44:43 +0800

    实现图片显示效果

--- gallery/src/components/Main.js
+++ gallery/src/components/Main.js
@@ -1,6 +1,7 @@
 require('styles/App.scss');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // let yeomanImage = require('../images/yeoman.png');
 
@@ -17,18 +18,168 @@ imageData = (function genImageURL(imageData){
 	return imageData;
 })(imageData);
 
-class AppComponent extends React.Component {
-  render() {
+var ImgFigure = React.createClass({
+	render:function(){
+		var styleObj = {};
+		if(this.props.arrange.pos){
+			styleObj = this.props.arrange.pos;
+		}
+		return(
+			<figure className="img-figure" style={styleObj}>
+				<img src={this.props.data.imageURL} alt={this.props.data.title} width="240px" height="240px"/>
+				<figcaption>
+					<h2 className="img-title">{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		)
+	}
+});
+
+function getRangeRandom(low, high){
+	return Math.floor(Math.random()*(high - low) + low);
+}
+
+var AppComponent = React.createClass ({
+	getInitialState:function(){
+		return {
+			imgsArrangeArr:[]
+		}
+	},
+	Constant:{
+		centerPos:{
+			left:0,
+			right:0
+		},
+		hPosRange:{
+			leftSecX:[0,0],
+			rightSecX:[0,0],
+			y:[0,0]
+		},
+		vPosRange:{
+			topY:[0,0],
+			x:[0,0]
+		}
+	},
+	componentDidMount:function(){
+		var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+				stageW = stageDOM.scrollWidth,
+				stageH = stageDOM.scrollHeight,
+				halfStageW = Math.ceil(stageW / 2),
+				halfStageH = Math.ceil(stageH / 2);
+
+		var imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+				imgW = imgFigureDOM.scrollWidth,
+				imgH = imgFigureDOM.scrollHeight,
+				halfImgW = Math.ceil(imgW / 2),
+				halfImgH = Math.ceil(imgH / 2);
+
+		this.Constant.centerPos = {
+			left:halfStageW - halfImgW,
+			top:halfStageH - halfImgH
+		}
+
+		this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+		this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+		this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+		this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+		this.Constant.hPosRange.y[0] = -halfImgH;
+		this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+		this.Constant.vPosRange.topY[0] = -halfImgH;
+		this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+		this.Constant.vPosRange.x[0] = halfStageW - imgW;
+		this.Constant.vPosRange.x[1] = halfStageW;
+
+		this.rearrange(0);
+	},
+	/*
+	 * 重新布局所有图片
+	 * @param centerIndex 制定排布哪个图片
+	 */
+	rearrange:function(centerIndex){
+		var imgsArrangeArr = this.state.imgsArrangeArr,
+				Constant = this.Constant,
+				centerPos = Constant.centerPos,
+				hPosRange = Constant.hPosRange,
+				vPosRange = Constant.vPosRange,
+				hPosRangeLeftSecX = hPosRange.leftSecX,
+				hPosRangeRightSecX = hPosRange.rightSecX,
+				hPosRangeY = hPosRange.y,
+				vPosRangeTopY = vPosRange.topY,
+				vPosRangeX = vPosRange.x,
+
+				imgsArrangeTopArr = [],
+				topImgNum = Math.floor(Math.random() * 2),
+				topImgSpliceIndex = 0,
+				
+				//布局居中位置元素
+				imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+				imgsArrangeCenterArr[0].pos = centerPos;
+
+				//布局上侧
+				topImgSpliceIndex = Math.floor(Math.random() * (imgsArrangeArr.length - topImgNum));
+				imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+
+				imgsArrangeTopArr.forEach(function(v,index){
+					imgsArrangeTopArr[index].pos = {
+						top:getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+						left:getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+					}
+				});
+
+				//布局两侧
+				for (var i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
+					var hPosRangeLORX = null;
+					
+					//前半部分布局左边，后半部分布局右边
+					if(i < k){
+						hPosRangeLORX = hPosRangeLeftSecX;
+					}else{
+						hPosRangeLORX = hPosRangeRightSecX
+					}
+
+					imgsArrangeArr[i].pos = {
+						top:getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+						left:getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+					}
+				}
+
+				if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
+					imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
+				}
+
+				imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
+
+				this.setState({
+					imgsArrangeArr:imgsArrangeArr
+				});
+	},
+  render:function() {
+  	var controllerUnits = [], imgFigures = [];
+
+  	imageData.forEach(function(v,index){
+  		if(!this.state.imgsArrangeArr[index]){
+  			this.state.imgsArrangeArr[index] = {
+  				pos:{
+  					left:0,
+  					top:0
+  				}
+  			}
+  		}
+  		imgFigures.push(<ImgFigure key={index} data={v} ref={'imgFigure' + index} arrange={this.state.imgsArrangeArr[index]}/>);
+  	}.bind(this));
+
     return (
-      <div className="stage">
-      	<section className="img-sec"></section>
-      	<nav className="controller-nav"></nav>
+      <div className="stage" ref="stage">
+      	<section className="img-sec">
+      		{imgFigures}
+      	</section>
+      	<nav className="controller-nav">
+      		{controllerUnits}
+      	</nav>
       </div>
     );
   }
-}
-
-AppComponent.defaultProps = {
-};
+});
 
 export default AppComponent;
