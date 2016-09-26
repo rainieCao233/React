f9f31aebfd6495cab414e81a79d1facf13b3741b
ycao <caoyue@eastmoney.com>
2016-09-26 15:39:06 +0800

    init project structure

--- gallery/src/components/Main.js
+++ gallery/src/components/Main.js
@@ -1,16 +1,28 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+
+// 获取图片相关的数据
+let imageData = require('../data/imageData.json');
+
+// 使用自执行函数，将图片名信息转成图片URL路径信息
+imageData = (function genImageURL(imageData){
+	for (var i = imageData.length - 1; i >= 0; i--) {
+		var singleImageData = imageData[i];
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		imageData[i] = singleImageData;
+	}
+	return imageData;
+})(imageData);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="stage">
+      	<section className="img-sec"></section>
+      	<nav className="controller-nav"></nav>
       </div>
     );
   }
